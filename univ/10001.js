a174e5f07ac905ac856ef232484f1eb764d97211
galaxy-yuhaochen <31811192+galaxy-yuhaochen@users.noreply.github.com>
2018-08-13 18:02:48 +0800

    /univ/10001.js
    add Xi'an Jiaotong University

---
+++ univ/10001.js
@@ -0,0 +1,14 @@
+var univ_list = {
+    school: "西安交大",                        // 学校名称
+    id: "10001",                               // 学校 ID
+    link: [ {
+        name: "西安交大主页",                      // 链接名称少于8个汉字或其等宽字符
+        url: "https://http://www.xjtu.edu.cn/"   // 链接地址
+    }, {
+        name: "教务处",
+        url: "https://dean.xjtu.edu.cn/
+    }, {
+        name: "国际处",                        // 链接数量小于或等于7个
+        url: "https://international.xjtu.edu.cn/"
+    } ]
+};
